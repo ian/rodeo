bfa96a13bdffe683a495259ee60302c9030ef555
JT <jason.s.theobald@gmail.com>
2021-03-20 14:57:35 -0700

    Fix wallet address prompt.

--- packages/giddyup/bin/giddyup.ts
+++ packages/giddyup/bin/giddyup.ts
@@ -6,8 +6,18 @@ import fsUtils from "fs-utils"
 import chalk from "chalk"
 import ora from "ora"
 import spawn from "cross-spawn"
-
 import inquirer from "inquirer"
+
+
+const isEthAddress = (address) => {
+    if ((/^(0x){1}[0-9a-fA-F]{40}$/i.test(address)) == false) {
+        console.log('Enter a valid wallet address.')
+        return process.exit(1)
+    } else {
+        return address
+    }
+}
+
 ;(async () => {
   console.log()
   console.log("RODEO BANNER")
@@ -31,6 +41,9 @@ import inquirer from "inquirer"
       const projectName = slugify(answers.projectName, {
         remove: /[!@#$%^&*()_+|}{:"?><\[\];',./}]/g,
       })
+
+      const walletAddress = isEthAddress(answers.walletAddress)
+
       const projectDir = `${dir}/${projectName}`
 
       const initializing = ora(`Initializing Project ${projectName}`).start()
