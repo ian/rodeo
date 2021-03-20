82cf4ef7290073472d406e26fac682c252952b13
Ian Hunter <ianhunter@gmail.com>
2021-03-20 11:43:36 -0500

    install script working

--- packages/giddyup/bin/giddyup.ts
+++ packages/giddyup/bin/giddyup.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-import fs from "fs"
+import fs from "fs-extra"
+import slugify from "slugify"
 import fsUtils from "fs-utils"
-import cpFile from "cp-file"
 import ora from "ora"
+import spawn from "cross-spawn"
 
 import inquirer from "inquirer"
 ;(async () => {
@@ -16,25 +17,55 @@ import inquirer from "inquirer"
       },
     ])
     .then(async (answers) => {
-      const { projectName } = answers
       const dir = process.cwd()
+      const projectName = slugify(answers.projectName, {
+        remove: /[!@#$%^&*()_+|}{:"?><\[\];',./}]/g,
+      })
       const projectDir = `${dir}/${projectName}`
 
-      const initializing = ora("Initializing Project").start()
+      const initializing = ora(`Initializing Project ${projectName}`).start()
 
-      // console.log(
-      //   "copying",
-      //   `${__dirname}/../rodeo.js`,
-      //   "to",
-      //   `${projectDir}/rodeo.js`
-      // )
+      await fs
+        .copy(`${__dirname}/../template/rodeo.js`, `${projectDir}/rodeo.js`)
+        .catch(console.log)
 
-      await cpFile(
-        `${__dirname}/../template/rodeo.js`,
-        `${projectDir}/rodeo.js`
+      await fs
+        .copy(`${__dirname}/../template/styles`, `${projectDir}/styles`)
+        .catch(console.log)
+
+      await fs
+        .copy(`${__dirname}/../template/site`, `${projectDir}/site`)
+        .catch(console.log)
+
+      await fsUtils.writeFileSync(
+        `${projectDir}/package.json`,
+        `
+{
+  "name": "${projectName}",
+  "version": "0.0.1",
+  "description": "",
+  "scripts": {
+    "build": "rodeo build",
+    "dev": "rodeo dev"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "",
+  "devDependencies": {
+    "@giddyup/cli": "^0.0.5"
+  }
+}`
       )
 
       initializing.succeed("DONE")
+
+      process.chdir(`./${projectName}`)
+
+      await spawnAsync(`yarn`, ["install"], {
+        stdio: "inherit",
+      }).catch((err) => {
+        console.error(err)
+      })
     })
     .catch((error) => {
       if (error.isTtyError) {
@@ -44,3 +75,20 @@ import inquirer from "inquirer"
       }
     })
 })()
+
+async function spawnAsync(program, args, options) {
+  options = (Array.isArray(args) ? options : args) || {}
+  args = Array.isArray(args) ? args : []
+  const code = await new Promise((resolve, reject) => {
+    const cp = spawn(program, args, options)
+    cp.on("error", (ex) => reject(ex))
+    cp.on("close", (code) => resolve(code))
+  })
+  if (code !== 0) {
+    throw new Error(
+      `${program}${
+        args.length ? ` ${JSON.stringify(args)}` : ""
+      } exited with non-zero code ${code}.`
+    )
+  }
+}
