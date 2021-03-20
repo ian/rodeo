6041fd372b317e272f5fb8d48b3113e41f7fe349
Ian Hunter <ianhunter@gmail.com>
2021-03-19 20:57:22 -0500

    add missing bin and commands

---
+++ packages/giddyup/commands/dev.ts
@@ -0,0 +1,33 @@
+import chokidar from "chokidar"
+import debounce from "debounce"
+import ora from "ora"
+import { spawn } from "child_process"
+
+let server
+
+const handleDirChange = debounce(() => {
+  console.clear()
+  const compiling = ora("Compiling").start()
+
+  if (server) {
+    server.kill("SIGINT")
+  }
+
+  spawn("tsc", ["--project", "tsconfig.json"], {
+    stdio: ["inherit", "inherit", "inherit"],
+  }).on("close", async (code) => {
+    if (code === 0) {
+      compiling.succeed()
+
+      server = spawn("node", [`${__dirname}/../lib/devServer.js`], {
+        stdio: "inherit",
+      })
+    } else {
+      compiling.fail("Error: Compilation failed. Watching for changes")
+    }
+  })
+}, 300)
+
+export default () => {
+  chokidar.watch("./rel", { persistent: true }).on("all", handleDirChange)
+}
