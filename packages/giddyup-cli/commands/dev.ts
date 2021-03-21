762076eefb77f45e01707c9c42e50957369c9abd
Ian Hunter <ianhunter@gmail.com>
2021-03-21 08:35:07 -0500

    don't open browser on launch

--- packages/giddyup-cli/commands/dev.ts
+++ packages/giddyup-cli/commands/dev.ts
@@ -17,7 +17,7 @@ async function startServer() {
   var params = {
     port: 8080, // Set the server port. Defaults to 8080.
     root: "./dist", // Set root directory that's being served. Defaults to cwd.
-    open: true, // When false, it won't load your browser by default.
+    open: false, // When false, it won't load your browser by default.
     // ignore: 'scss,my/templates', // comma-separated string for paths to ignore
     // file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
     wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
