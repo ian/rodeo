8e4d48c78bab2f27259121a0b3b8fbdf828ce91c
Ian Hunter <ianhunter@gmail.com>
2021-03-21 08:47:54 -0500

    fix build script to actually build css

--- packages/giddyup-cli/commands/build.ts
+++ packages/giddyup-cli/commands/build.ts
@@ -1,22 +1,51 @@
 import ora from "ora"
 import { spawn } from "child_process"
 
-export default () => {
-  const compiling = ora("Compiling").start()
+export default async () => {
+  console.log("Building ...")
 
-  spawn(
-    `${__dirname}/../../node_modules/.bin/eleventy`,
-    ["--config", `rodeo.js`],
-    {
+  const invocations = [
+    [
+      `${__dirname}/../../node_modules/.bin/postcss`,
+      "styles/main.css",
+      "-d",
+      "dist/css/",
+      "--config",
+      `${__dirname}/../postcss.config.js`,
+    ],
+    [
+      `${__dirname}/../../node_modules/.bin/eleventy`,
+      "--quiet",
+      "--config",
+      `rodeo.js`,
+    ],
+  ]
+  for (const [program, ...args] of invocations) {
+    await spawnAsync(program, args, {
       stdio: "inherit",
-    }
-  ).on("close", (code) => {
-    if (code === 0) {
-      compiling.succeed()
-    } else if (code === 1) {
-      compiling.warn()
-    } else {
-      compiling.fail()
-    }
+    }).catch((err) => {
+      console.error(err)
+    })
+  }
+
+  console.log("DONE")
+  console.log()
+}
+
+// @todo - dry this up
+async function spawnAsync(program, args, options) {
+  options = (Array.isArray(args) ? options : args) || {}
+  args = Array.isArray(args) ? args : []
+  const code = await new Promise((resolve, reject) => {
+    const cp = spawn(program, args, options)
+    cp.on("error", (ex) => reject(ex))
+    cp.on("close", (code) => resolve(code))
   })
+  if (code !== 0) {
+    throw new Error(
+      `${program}${
+        args.length ? ` ${JSON.stringify(args)}` : ""
+      } exited with non-zero code ${code}.`
+    )
+  }
 }
