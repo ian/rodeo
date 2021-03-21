11e7a53786edb111f61d8c536ed3724ecd8d5da4
Ian Hunter <ianhunter@gmail.com>
2021-03-20 20:01:00 -0500

    cleanup server, hot module reloading now

--- packages/giddyup-cli/commands/dev.ts
+++ packages/giddyup-cli/commands/dev.ts
@@ -1,17 +1,32 @@
 import chokidar from "chokidar"
 import debounce from "debounce"
 import spawn from "cross-spawn"
+import server from "live-server"
 
 async function startServer() {
-  return spawnAsync(
-    `${__dirname}/../../node_modules/.bin/http-server`,
-    ["./dist"],
-    {
-      stdio: "inherit",
-    }
-  ).catch((err) => {
-    console.error(err)
-  })
+  // return spawnAsync(
+  //   `${__dirname}/../../node_modules/.bin/http-server`,
+  //   ["./dist"],
+  //   {
+  //     stdio: "inherit",
+  //   }
+  // ).catch((err) => {
+  //   console.error(err)
+  // })
+
+  var params = {
+    port: 8080, // Set the server port. Defaults to 8080.
+    root: "./dist", // Set root directory that's being served. Defaults to cwd.
+    open: true, // When false, it won't load your browser by default.
+    // ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+    // file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+    wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+    // mount: [['/components', './node_modules']], // Mount a directory to a route.
+    logLevel: 0, // 0 = errors only, 1 = some, 2 = lots
+    // middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+  }
+
+  server.start(params)
 }
 
 const handleStyleChange = debounce(async (event) => {
